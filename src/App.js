35dd3baadc1c0a790f04430460673f32d29342fb
Łukasz Klekner <lukaszklekner@gmail.com>
2022-06-07 20:40:34 +0200

    adds structure and styles of the app to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import './App.css'
 function App() {
   return (
     <div>
-      Pomodoro App
+      <h1>Pomodoro App</h1>
+      <hr />
+      <div className='App'>
+        <div className='TimeboxEditor'>
+          <label>
+            Co robisz?
+            <input type='text' value='Uczę się Reacta' />
+          </label>
+          <br />
+          <label>
+            Ile minut?
+            <input type='number' value='25' />
+          </label>
+          <br />
+          <button>Zacznij!</button>
+        </div>
+      </div>
+      <div className='Timebox'>
+        <h1>Uczę się Reacta</h1>
+        <h2 className='Clock'>Pozostało 15:15</h2>
+        <div className='ProgressBar inactive'>
+          <div style={{ width: '45%' }}></div>
+        </div>
+        <button>Start</button>
+        <button>Stop</button>
+        <button>Pauzuj</button>
+        Liczba przerw: 5
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
