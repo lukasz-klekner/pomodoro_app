f590814ca8d6e56e5cce9c43176695b83e459ec5
Łukasz Klekner <lukaszklekner@gmail.com>
2022-06-10 16:35:22 +0200

    adds states to Timebox and implements ms

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,8 +1,7 @@
 import { clamp, prefixWithZeros } from '../utils/helpers'
 
-const Clock = ({ className, hours, minutes, seconds, miliseconds }) => {
+const Clock = ({ className, miliseconds, minutes, seconds }) => {
   const time = {
-    hours: prefixWithZeros(clamp(0, hours, 23), 2),
     minutes: prefixWithZeros(clamp(0, minutes, 59), 2),
     seconds: prefixWithZeros(clamp(0, seconds, 59), 2),
     miliseconds: prefixWithZeros(clamp(0, miliseconds, 999), 3),
@@ -10,7 +9,7 @@ const Clock = ({ className, hours, minutes, seconds, miliseconds }) => {
 
   return (
     <h2 className={`Clock ${className}`}>
-      Pozostało {time.hours}:{time.minutes}:{time.seconds}:{time.miliseconds}
+      Pozostało {time.minutes}:{time.seconds}:{time.miliseconds}
     </h2>
   )
 }
--- src/components/Timebox.js
+++ src/components/Timebox.js
@@ -1,46 +1,91 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import Clock from './Clock'
 import ProgressBar from './ProgressBar'
 
+let intervalId = null
+
 const Timebox = () => {
+  const [elapsedTimeInMiliseconds, setelapsedTimeInMiliseconds] = useState(0)
   const [isRunning, setIsRunning] = useState(false)
   const [isPaused, setIsPaused] = useState(false)
   const [pausesCount, setPausesCount] = useState(0)
 
+  const totalTimeInMiliseconds = 30 * 1000
+  const timeLeftInMiliseconds =
+    totalTimeInMiliseconds - elapsedTimeInMiliseconds
+  const minutesLeft = Math.floor(timeLeftInMiliseconds / (60 * 1000))
+  const secondsLeft = Math.floor((timeLeftInMiliseconds / 1000) % 60)
+  const milisecondsLeft = Math.floor(timeLeftInMiliseconds % 1000)
+  const progressInPercent =
+    (elapsedTimeInMiliseconds / totalTimeInMiliseconds) * 100.0
+
+  const startTimer = () => {
+    intervalId = window.setInterval(() => {
+      setelapsedTimeInMiliseconds((prevState) => prevState + 10)
+    }, 10)
+  }
+
+  const stopTimer = () => window.clearInterval(intervalId)
+
   const handleStart = () => {
     setIsRunning(true)
+    setIsPaused(false)
+
+    startTimer()
   }
 
   const handleStop = () => {
     setIsRunning(false)
     setIsPaused(false)
     setPausesCount(0)
+    setelapsedTimeInMiliseconds(0)
+
+    stopTimer()
   }
 
   const togglePause = () => {
-    setIsPaused((prevState) => !prevState)
+    setIsPaused((prevState) => {
+      const isPaused = !prevState
+      if (isPaused) {
+        stopTimer()
+        setIsRunning(false)
+      } else {
+        startTimer()
+        setIsRunning(true)
+      }
+
+      return isPaused
+    })
     setPausesCount((prevState) => (!isPaused ? prevState + 1 : prevState))
   }
+
+  useEffect(() => {
+    if (totalTimeInMiliseconds - elapsedTimeInMiliseconds < 0) {
+      handleStop()
+    }
+  }, [elapsedTimeInMiliseconds, totalTimeInMiliseconds])
 
   return (
     <div className='Timebox'>
       <h1>Uczę się Reacta</h1>
       <Clock
         className={isPaused ? 'inactive' : ''}
-        hours={27}
-        minutes={700}
-        seconds={120}
-        miliseconds={1000}
+        miliseconds={milisecondsLeft}
+        minutes={minutesLeft}
+        seconds={secondsLeft}
       />
-      <ProgressBar className={isPaused ? 'inactive' : ''} percent={25} />
-      <button disabled={isRunning} onClick={handleStart}>
+      <ProgressBar
+        className={isPaused ? 'inactive' : ''}
+        percent={progressInPercent}
+      />
+      <button disabled={isRunning !== isPaused} onClick={handleStart}>
         Start
       </button>
       <button disabled={!isRunning} onClick={handleStop}>
         Stop
       </button>
-      <button disabled={!isRunning} onClick={togglePause}>
+      <button disabled={isRunning === isPaused} onClick={togglePause}>
         {isPaused ? 'Wznów' : 'Pauzuj'}
       </button>
       Liczba przerw: {pausesCount}
