2f17265fc673e6fa5ea83ff34ca5b08d36e9d538
Łukasz Klekner <lukaszklekner@gmail.com>
2022-06-11 09:22:08 +0200

    adds state to TimeboxEditor component

--- src/components/TimeboxEditor.js
+++ src/components/TimeboxEditor.js
@@ -1,17 +1,34 @@
-const TimeboxEditor = () => (
-  <div className='TimeboxEditor'>
-    <label>
-      Co robisz?
-      <input type='text' value='Uczę się Reacta' />
-    </label>
-    <br />
-    <label>
-      Ile minut?
-      <input type='number' value='25' />
-    </label>
-    <br />
-    <button>Zacznij!</button>
-  </div>
-)
+import { useState } from 'react'
+
+const TimeboxEditor = () => {
+  const [title, setTitle] = useState('')
+  const [totalTimeInMiliseconds, setTotalTimeInMiliseconds] = useState('')
+
+  const handleTitleChange = (event) => setTitle(event.target.value)
+  const handleTotalTimeInMilisecondsChange = (event) =>
+    setTotalTimeInMiliseconds(Number(event.target.value))
+
+  console.log(totalTimeInMiliseconds)
+
+  return (
+    <div className='TimeboxEditor'>
+      <label>
+        Co robisz?
+        <input type='text' value={title} onChange={handleTitleChange} />
+      </label>
+      <br />
+      <label>
+        Ile minut?
+        <input
+          type='number'
+          value={totalTimeInMiliseconds}
+          onChange={handleTotalTimeInMilisecondsChange}
+        />
+      </label>
+      <br />
+      <button>Zacznij!</button>
+    </div>
+  )
+}
 
 export default TimeboxEditor
