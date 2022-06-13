d2658fa254c10938fb98006400db39add70cfd88
Łukasz Klekner <lukaszklekner@gmail.com>
2022-06-13 20:13:56 +0200

    uses BEM to style ProgressBar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,12 +1,13 @@
 const ProgressBar = ({ className, percent, trackRemaining = false }) => (
   <div
-    className={`ProgressBar ${className}`}
+    className={`progress progress--big ${className}`}
     style={{
       display: 'flex',
       justifyContent: trackRemaining ? 'flex-end' : 'flex-start',
     }}
   >
     <div
+      className='progress__bar progress__bar--color-green'
       style={{ width: `${trackRemaining ? 100 - percent : percent}%` }}
     ></div>
   </div>
